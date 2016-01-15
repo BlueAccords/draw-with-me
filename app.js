77f5782e0de81a830299fec9e47b3b009961992e
BlueAccords <myoldemailname@gmail.com>
2016-01-15 14:37:23 -0800

    added routes for photo controller and beginning image upload

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// formidable used to handle file uploads.
+// http > https://nodejs.org/api/http.html
+// util > https://nodejs.org/api/util.html
+var formidable = require('formidable'),
+  http = require('http');
+  util = require('util');
+
+// routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
+//var photos = require('./routes/photos');
 
 var app = express();
 
@@ -23,7 +32,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/photos', photos);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
