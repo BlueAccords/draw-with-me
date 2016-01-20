40e49d3dd1b73fd39533abfaa49ed205a5817a3c
BlueAccords <myoldemailname@gmail.com>
2016-01-20 13:28:11 -0800

    added photos route + updated schema in readme

--- README.md
+++ README.md
@@ -2,10 +2,9 @@ Draw With Me
 ======
 A web app that allows creations of public and private rooms where you can upload art.  
 
-### TODO
+## TODO
 --------
 * CRUD(Create, Read, Update, Delete) of images to the database
-  * Integrate Image storage with google drive.
 * User Authentication
   * OAuth through google, reddit, github.
 * User Authorization
@@ -14,3 +13,48 @@ A web app that allows creations of public and private rooms where you can upload
   * URL generation for rooms
   * Generate private room keys
   * Room authorization restrictions
+
+## CRUD, Schema Design
+#### Users
+* username
+* password
+* email
+* rooms they own/created
+* rooms they are members of
+* personal collection
+  * images
+* friends
+* inbox
+  * messages
+    * author
+    * body
+    * timestamp  
+
+#### Rooms
+* collections
+  * (group can store images in there)
+* owner
+  * transferrable
+* moderators
+  * (mods can have similar privileges to owner)
+* members
+  * rank
+    * permission levels to edit room
+* events
+  * allows scheduling of events
+  * Ability to send out emails to members
+* public/private
+  * public allows public to view or public to contribute to.
+  * owner can choose to authorize image uploads or not.
+  * keys/passwords to join if private
+  * link to room(allow custom slugs)  
+
+### Images
+* filename
+* file size
+  * limit file size(need to think of a max size)
+* filetype
+  * limit file types?
+* date
+* author  
+* tags(important)
--- routes/photos.js
+++ routes/photos.js
@@ -13,8 +13,57 @@ router.get('/', function(req, res){
   res.render('photos', {title: 'Draw With Me - Photos'});
 });
 
-router.post('/upload', function(req, res){
+router.post('/upload', function(req, res, next){
+  var form = new formidable.IncomingForm();
 
+  // parse the form.
+  form.parse(req, function(err, fields, files){
+    // 'file' = name of the input field's type file
+    console.log(files);
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.write('received upload:\n\n');
+    res.end(util.inspect({fields: fields, files: files}));
+  });
+
+  // error handling
+  form.on('error', function(err){
+    console.error(err);
+  });
+
+  // display progress to console.
+  form.on('progress', function(bytesReceived, bytesExpected){
+    var percentComplete = (bytesReceived / bytesExpected) * 100;
+    console.log(percentComplete.toFixed(2));
+  });
+
+  // file upload complete handling
+  form.on('end', function(fields, files){
+    // location of temporary uploaded file.
+    var temp_path = this.openedFiles[0].path;
+    // the file name of the uploaded file.
+    var file_name = this.openedFiles[0].name;
+    // Location where we'll copy the uploaded file.
+    var new_location = 'public/images';
+
+    fs.copy(temp_path, new_location + file_name, function(err){
+      if(err) {
+        console.error(err);
+      } else {
+        console.log("success");
+      }
+    });
+    // fs.readFile(temp_path, function(err, data){
+    //   fs.writeFile(new_location + file_name, data, function(err){
+    //     fs.unlink(temp_path, function(err){
+    //       if(err){
+    //         console.log(err);
+    //       } else {
+    //         console.log("File successfully uploaded");
+    //       }
+    //     });
+    //   });
+    // });
+  });
 });
 
 // export
