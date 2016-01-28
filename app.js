f00068c6e1fe673077729eaab88f7069cacc0ee0
BlueAccords <myoldemailname@gmail.com>
2016-01-28 10:27:22 -0800

    refactored authentication

--- app.js
+++ app.js
@@ -1,40 +1,67 @@
 var express = require('express');
+var app = express();
 var path = require('path');
-var favicon = require('serve-favicon');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
+var session       = require('express-session');
 
+var configDB = ('./config/database');
 
 // routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var photos = require('./routes/photos');
 
-var app = express();
 
-// nunjucks config
+// middleware configuration
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport);
+
+// view engine setup
 nunjucks.configure('views', {
   autoescape: true,
   express: app
 });
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// ===============================================================
+// MIDDLEWARE =====================================================
+// =================================================================
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// session setup/configuration.
+// TODO: setup mongodb connection(look at express-session docs)
+app.use(session({
+  secret: 'lostconnectiontosteamwillreconnectshortly',
+  //store: mongoconnect,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+//passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
+// ===============================================================
+// ROUTES =====================================================
+// =================================================================
 app.use('/', routes);
 app.use('/photos', photos);
-app.use('/', users);
+require(users)(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -67,5 +94,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
+// export to bin/www which launches the server.
 module.exports = app;
---
+++ config/passport.js
@@ -0,0 +1,118 @@
+// load strategies
+var LocalStrategy = require('passport-local').Strategy;
+var GoogleStrategy = require('passport-google-oauth').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var TwitterStrategy = require('passport-twitter').Strategy;
+
+// load user model.
+var User = require('../models/user');
+
+// export passport as function module
+module.exports = function(passport) {
+  // passport session setup.
+  // passport serializes user info upon login and creates a cookie
+  // then deserializes them upon each authentication request.
+  // You can set which part of the user info is serialized.
+
+  // serialization
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  // deserialize
+  // searches mongoDB for user by id
+  // scrubs user password then stores user Object as a cookie.
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      user.local.passport = undefined;
+      done(err, user);
+    });
+  });
+  //============================================================================
+  // LOCAL SIGNUP============================================================
+  // ===========================================================================
+
+  // changing default check to email/password.
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true // enable passing of the request to callback.
+  },
+  function(req, email, password, done) {
+
+    // async
+    // User.findOne won't execute until data is sent back.
+    process.nextTick(function() {
+
+      // TODO: refactor local strategy to find username/email in model.
+      // find a user whose email/username match the posted info.
+      var critera = {$or: [{'local.username' : email},
+                          {'local.email'    : email}]};
+      // search for user/email and return found user
+      User.findOne(critera, function(err, user) {
+        // checks for and returns any errors
+        if(err) {
+          return done(err);
+        }
+
+        // check to see if email/username is taken.
+        if(user) {
+          return done(null, false, req.flash('signupMessage'),
+                      'That Email/Username is already taken');
+        } else {
+          var newUser = new User();
+
+          // setup User credientials and save to db.
+          newUser.local.username = req.body.username;
+          newUser.local.email    = email;
+          newUser.local.password = newUser.generateHash(password);
+
+          // save user to db
+          newUser.save(function(err) {
+            if(err) {
+              throw err;
+            }
+
+            // return user
+            return done(null, newUser);
+          });
+        }
+
+      });
+    });
+  }));
+
+  // LOCAL LOGIN ====================================================
+  passport.use('local-login', new LocalStrategy({
+    //override default of username/password
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true // pass back request to callback function
+  },
+  function(req, email, password, done) {
+
+    // find user via email OR username.
+    // check if user trying to login already exists
+    var critera = {$or: [{"local.username" : email},
+                        {"local.email"    : email}]};
+
+    User.findOne(critera, function(err, user) {
+
+      // returns errors if any
+      if (err){
+        return done(err);
+      }
+
+      // if no user is found OR password is invalid return the same message
+      // more secure to display static message(though people could just try
+      // to register with the email to find out anyways. meh.)
+      if(!user || !user.validPassword(password)){
+        return done(null, false, req.flash('loginMessage',
+                                'Invalid Email/Username or Password Combination'));
+      }
+      // finished authentication, return successful user.
+      return done(null, user);
+
+    });
+  }));
+};
